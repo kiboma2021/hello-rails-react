11f19442a737ca9a97f136e4ede683e8717b980c
kiboma <omayioben@gmail.com>
2024-02-14 11:39:29 +0300

    Fix linter errors

--- app/javascript/react/src/components/Home.jsx
+++ app/javascript/react/src/components/Home.jsx
@@ -1,28 +1,26 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
 const Home = () => {
-    const [data, getData]=React.useState([])
-    const url = 'http://127.0.0.1:3000/random_greeting'
-    React.useEffect(() =>{
-        async function fetchGreeting() {
-            const response = await fetch(url)
-            const result = await response.json()
-            getData(result)
-        }
-        fetchGreeting();
-    },[url])
+  const [data, getData] = React.useState([]);
+  const url = 'http://127.0.0.1:3000/random_greeting';
+  React.useEffect(() => {
+    async function fetchGreeting() {
+      const response = await fetch(url);
+      const result = await response.json();
+      getData(result);
+    }
+    fetchGreeting();
+  }, [url]);
 
   return (
     <div>
-        <h2>{data.message}</h2>
-     
-    </div>
-  )
-}
+      <h2>{data.message}</h2>
 
-document.addEventListener('DOMContentLoaded',() => 
-    ReactDOM.render(<Home />, document.getElementById('root'))
-)
+    </div>
+  );
+};
+
+document.addEventListener('DOMContentLoaded', () => ReactDOM.render(<Home />, document.getElementById('root')));
 
 export default Home;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const path    = require("path")
-const webpack = require("webpack")
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  mode: "production",
-  devtool: "source-map",
+  mode: 'production',
+  devtool: 'source-map',
   entry: {
-    application: "./app/javascript/application.js"
+    application: './app/javascript/application.js',
   },
   module: {
     rules: [
@@ -13,23 +13,23 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
       },
     ],
   },
   output: {
-    filename: "[name].js",
-    sourceMapFilename: "[file].map",
-    chunkFormat: "module",
-    path: path.resolve(__dirname, "app/assets/builds"),
+    filename: '[name].js',
+    sourceMapFilename: '[file].map',
+    chunkFormat: 'module',
+    path: path.resolve(__dirname, 'app/assets/builds'),
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
-  ]
-}
+      maxChunks: 1,
+    }),
+  ],
+};
